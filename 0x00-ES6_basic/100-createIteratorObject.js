b5f7b07a82afd276dc366a229e3e54574f4adce3
HazemUsama <hazemosama681@gmail.com>
2024-05-30 22:46:14 +0300

    last fix

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -3,18 +3,21 @@ export default function createIteratorObject(report) {
     [Symbol.iterator]() {
       const employees = [];
       for (const department in report.allEmployees) {
-        employees.push(...report.allEmployees[department]);
+        if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
+          employees.push(...report.allEmployees[department]);
+        }
       }
 
       let currIdx = 0;
       return {
         next() {
-          if (currIdx < employees.length)
-            return { value: employees[currIdx++], done: false };
-          else
-            return {done: true};
-        }
-      }
-    }
+          if (currIdx < employees.length) {
+            currIdx += 1;
+            return { value: employees[currIdx - 1], done: false };
+          }
+          return { done: true };
+        },
+      };
+    },
   };
 }
