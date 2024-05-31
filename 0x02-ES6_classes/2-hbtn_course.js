6915e30f2e6824eb03e75ebcfd295006d1902d83
HazemUsama <hazemosama681@gmail.com>
2024-05-31 03:32:31 +0300

    getters and setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  get lenght() {
+    return this._lenght;
+  }
+
+  set lenght(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._lenght = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value)) {
+      throw new TypeError('Students must be an array');
+    }
+    for (const student of value) {
+      if (typeof student !== 'string') {
+        throw new TypeError('Student must be a string');
+      }
+    }
+    this._students = value;
+  }
+}
