b2f5f8b7e2fd07b0a23ed6b644c17f4091885705
HazemUsama <hazemosama681@gmail.com>
2024-06-08 05:36:01 +0300

    TypeScript Done

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,38 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+function numberOfReports(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+interface IStudentClassConstructor {
+  new (firstName: string, lastName: string): IStudentClass;
+}
+
+interface IStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements IStudentClass {
+  private _firstName!: string;
+  private _lastName!: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this._firstName;
+  }
+}
