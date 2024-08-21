c5509dcbb433478b99650bcc384b838bb86dd10e
Hazem Osama <hazem@debian.168.1.20>
2024-08-21 05:11:53 +0300

    Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+function countStudents(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+      if (data) {
+        const students = data
+          .slice(1)
+          .trim()
+          .split('\n');
+
+        console.log(`Number of students: ${students.length}`);
+
+        const fieldCounts = new Map();
+
+        for (const student of students) {
+          const [firstName, lastName, age, field] = student.split(',');
+
+          if (!firstName || !lastName || !age || !field) return;
+          if (!fieldCounts.has(field)) {
+            fieldCounts.set(field, []);
+          }
+          fieldCounts.get(field).push(firstName);
+        }
+
+        fieldCounts.forEach((names, field) => {
+          console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+        });
+
+        resolve(true);
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
