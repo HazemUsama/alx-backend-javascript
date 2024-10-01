88306719ea59186806861a4bcad8d2dc5b84b02f
Hazem Osama <hazem@debian.168.1.20>
2024-10-01 18:41:40 +0300

    New start

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++
@@ -1,9 +0,0 @@
-import AppController from '../controllers/AppController.js';
-import StudentsController from '../controllers/StudentsController.js';
-
-
-export default function mapRoutes(app) {
-  app.get('/', AppController.getHomepage);
-  app.get('/students', StudentsController.getAllStudents);
-  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
-};
