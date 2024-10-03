b9c7857e1c4b3763e5947e08ac49c936652595d3
Hazem Osama <hazem@debian.168.1.20>
2024-10-03 21:45:36 +0300

    Add more tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -8,9 +8,26 @@ describe("calculateNumber", function() {
 
   it("Check float numbers", function() {
     assert.strictEqual(calculateNumber(2.5, 7.6), 11);
+    assert.strictEqual(calculateNumber(8.2, 3.5), 12);
   });
 
   it("Check for zero", function() {
     assert.strictEqual(calculateNumber(0, 0), 0);
+    assert.strictEqual(calculateNumber(0, 2.3), 2);
+  });
+
+  it("Check for mixed number `float` & `integer`", function() {
+    assert.strictEqual(calculateNumber(5, 6.3), 11);
+    assert.strictEqual(calculateNumber(2, 3.8), 6);
+  });
+
+  it("Check for negative numbers", function() {
+    assert.strictEqual(calculateNumber(-3, -6.3), -9);
+    assert.strictEqual(calculateNumber(-4, -2), -6);
+  });
+
+  it("Check for big numbers", function() {
+    assert.strictEqual(calculateNumber(10000000, 20000000), 30000000);
+    assert.strictEqual(calculateNumber(10000000.2, 20000000.3), 30000000);
   });
 });
