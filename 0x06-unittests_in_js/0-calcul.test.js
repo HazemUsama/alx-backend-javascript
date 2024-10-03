53c3b8dae33e6caf34e402cea15b8dd3f7d5e89d
Hazem Osama <hazem@debian.168.1.20>
2024-10-03 21:06:30 +0300

    Using strictEqual instead of Equal

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,14 +3,14 @@ const calculateNumber = require("./0-calcul.js");
 
 describe("calculateNumber", function() {
   it("Check simple sum", function() {
-    assert.equal(calculateNumber(3, 4), 7);
+    assert.strictEqual(calculateNumber(3, 4), 7);
   });
 
   it("Check float numbers", function() {
-    assert.equal(calculateNumber(2.5, 7.6), 11);
+    assert.strictEqual(calculateNumber(2.5, 7.6), 11);
   });
 
   it("Check for zero", function() {
-    assert.equal(calculateNumber(0, 0), 0);
+    assert.strictEqual(calculateNumber(0, 0), 0);
   });
 });
